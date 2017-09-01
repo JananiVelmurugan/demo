c32094890c924396147e362f6434cd3d7b22baeb
Janani <jananise@gmail.com>
2017-09-01 09:23:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("books")
+public class BookController {
+
+	@GetMapping("/")
+	public List<String> list() {
+		List<String> books = new ArrayList<>();
+		books.add("Java");
+		books.add("C");
+		return books;
+
+	}
+}
